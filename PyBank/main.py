302ecef8ef4ffd533c792ac22ad429727dc64094
Robert Boulka <robertboulka@live.com>
2021-01-21 22:11:06 -0600

    added sum of months total working and sum of column 1 working

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,27 @@
-print("Hello from PyBank!")
+import os
+import csv
+sum_total = 0
+row_count = 0
+csvpath = os.path.join("Resources", "budget_data.csv")
+
+with open(csvpath) as csvfile:
+        csvreader = csv.reader(csvfile, delimiter= ",")
+        print(csvreader)
+        #skip header for csv file
+        csv_header = next(csvreader)
+        #print Header
+        print(f"CSV Header: {csv_header}")
+        #print test csv
+        
+        for row in csvreader:
+            print(row)
+            sum_total += float(row[1])
+            row_count = row_count + 1
+
+
+        #row_count = len(list(csvreader))
+print(f"Financial Analysis")
+print(f"-----------------------------")
+print(f"Total Months:", row_count)
+print(f"Total:" , sum_total)
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,2 @@
-print("Hello from PyPoll!")
-
-
-a = 27
-print(f"here's the value of a: {a}")
+import os
+import csv
